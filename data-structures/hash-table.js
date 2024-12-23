702001f96b6268150cc822ab134a9adbb8825f12
Denis <d.cukert@tg.dunice.net>
2024-12-23 09:54:53 +0300

    fix: changed hash table

--- data-structures/hash-table.js
+++ data-structures/hash-table.js
@@ -1,6 +1,9 @@
 class HashTable {
   constructor(size = 50) {
     this.buckets = new Array(size).fill(null).map(() => new Map());
+    this.size = size;
+    this.count = 0;
+    this.loadFactorThreshold = 0.7;
   }
 
   _hash(key) {
@@ -8,14 +11,32 @@ class HashTable {
     let hash = 2166136261; // FNV offset basis
     for (let i = 0; i < keyStr.length; i++) {
       hash ^= keyStr.charCodeAt(i);
-      hash = (hash * 16777619) >>> 0; 
+      hash = (hash * 16777619) >>> 0;
     }
     return hash % this.buckets.length;
+  }
+
+  _resize(newSize) {
+    const oldBuckets = this.buckets;
+    this.buckets = new Array(newSize).fill(null).map(() => new Map());
+    this.size = newSize;
+    this.count = 0;
+
+    for (const bucket of oldBuckets) {
+      for (const [key, value] of bucket) {
+        this.add(key, value);
+      }
+    }
   }
 
   add(key, value) {
     const index = this._hash(key);
     this.buckets[index].set(key, value);
+    this.count++;
+
+    if (this.count / this.size > this.loadFactorThreshold) {
+      this._resize(this.size * 2);
+    }
   }
 
   get(key) {
@@ -25,7 +46,11 @@ class HashTable {
 
   remove(key) {
     const index = this._hash(key);
-    return this.buckets[index].delete(key);
+    const deleted = this.buckets[index].delete(key);
+    if (deleted) {
+      this.count--;
+    }
+    return deleted;
   }
 }
 
